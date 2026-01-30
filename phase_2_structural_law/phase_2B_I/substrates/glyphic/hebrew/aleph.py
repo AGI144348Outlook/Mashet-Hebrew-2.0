d82420a48c494548af2e219a0a1fa1e1f2437ebb
Timothy <AGI144348@outlook.com>
2026-01-30 12:49:05 -0600

    Update aleph.py

--- phase_2_structural_law/phase_2B_I/substrates/glyphic/hebrew/aleph.py
+++ phase_2_structural_law/phase_2B_I/substrates/glyphic/hebrew/aleph.py
@@ -1,1 +1,24 @@
+from .schema import HEBREW_GLYPH_SCHEMA
 
+ALEPH = {
+    "glyph_id": "aleph",
+    "glyph_set": "hebrew",
+    "substrate_type": "glyph",
+
+    "form_class": "composite",
+    "directionality": "none",
+    "closure": "open",
+
+    "allowed_domains": ["air"],
+
+    "allows_containment": False,
+    "allows_adjacency": True,
+    "allows_stacking": False,
+
+    "status": "inert",
+
+    "meaning": None,
+    "phonetic": None,
+    "numeric": None,
+    "behavior": None,
+}
